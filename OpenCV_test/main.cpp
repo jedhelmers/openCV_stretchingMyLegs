ea29da689e1989960fb7ed90f1de61f8fcee7b88
Jed Helmers <jedhelmers@gmail.com>
2018-01-01 19:34:16 -0500

    Initial Commit

---
+++ OpenCV_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV_test
+//
+//  Created by Jesse Helmers on 1/1/18.
+//  Copyright © 2018 Jesse Helmers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
